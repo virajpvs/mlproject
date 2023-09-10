89ccec7e515ffb9505e2739730d3ffbc3ba3d0da
Your Name <you@example.com>
2023-09-10 16:18:30 +0530

    EDA and problem statement

--- scr/logger.py
+++ scr/logger.py
@@ -6,8 +6,8 @@ from datetime import datetime
 logs_directory = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_directory, exist_ok=True)
 
-# Generate the log file name with a timestamp
-log_file_name = datetime.now().strftime('%m_%d_%Y_%H_%M_%S') + ".log"
+# Generate the log file name with a timestamp including milliseconds
+log_file_name = datetime.now().strftime('%m_%d_%Y_%H_%M_%S_%f') + ".log"
 
 # Specify the full path to the log file
 log_file_path = os.path.join(logs_directory, log_file_name)
@@ -15,11 +15,19 @@ log_file_path = os.path.join(logs_directory, log_file_name)
 # Configure logging
 logging.basicConfig(
     filename=log_file_path,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] [%(levelname)s] %(name)s - %(message)s",
     level=logging.INFO
 )
 
-# if __name__ == "__main__":
-#     logging.info("Logging has started")
+if __name__ == "__main__":
+    logging.info("Logging has started")
+
+    # # Example log messages
+    # logging.debug("This is a debug message")
+    # logging.info("This is an info message")
+    # logging.warning("This is a warning message")
+    # logging.error("This is an error message")
+    # logging.critical("This is a critical message")
+
 
 # python scr/logger.py
